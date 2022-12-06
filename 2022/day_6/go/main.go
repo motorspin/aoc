e7fc1c677d4b9f0dcd690e79f82800ced2d52399
Travis Farrell <tfarrell10@bloomberg.net>
2022-12-06 01:16:57 -0500

    Part 2 of Day 6 Solution

--- 2022/day_6/go/main.go
+++ 2022/day_6/go/main.go
@@ -51,6 +51,46 @@ func (s *LastFour) Position() int {
 	return s.pos
 }
 
+type LastFourteen struct {
+	s   []rune
+	pos int
+	m   map[rune]int
+}
+
+func NewLastFourteen() *LastFourteen {
+	return &LastFourteen{make([]rune, 14), 0, make(map[rune]int, 0)}
+}
+
+func (s *LastFourteen) Push(val rune) {
+	if s.pos < 14 {
+		s.s[s.pos] = val
+	} else {
+		s.m[s.s[0]] -= 1
+		s.s = s.s[1:]
+		s.s = append(s.s, val)
+	}
+	s.m[val] += 1
+	s.pos += 1
+}
+
+func (s *LastFourteen) IsStart() bool {
+	if s.pos < 14 {
+		return false
+	}
+
+	for _, val := range s.m {
+		if val > 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (s *LastFourteen) Position() int {
+	return s.pos
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -58,10 +98,21 @@ func main() {
 	buffer := scanner.Text()
 	lastFour := NewLastFour()
 
+	fmt.Printf("Part 1: ")
 	for i := 0; i < len(buffer); i++ {
 		lastFour.Push(rune(buffer[i]))
 		if lastFour.IsStart() {
 			fmt.Println(lastFour.Position())
+			break
+		}
+	}
+
+	lastFourteen := NewLastFourteen()
+	fmt.Printf("Part 2: ")
+	for i := 0; i < len(buffer); i++ {
+		lastFourteen.Push(rune(buffer[i]))
+		if lastFourteen.IsStart() {
+			fmt.Println(lastFourteen.Position())
 			break
 		}
 	}
